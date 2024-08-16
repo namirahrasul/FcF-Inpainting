1e19f074a82976cd3b3d9ec879c1cbeb70a2624a
namirahrasul <n.rsl.136@gmail.com>
2024-08-16 10:21:22 +0600

    attept 2 to only have 30 pics

--- tools/prepare_celebahq.sh
+++ tools/prepare_celebahq.sh
@@ -1,14 +1,20 @@
 mkdir -p datasets/celeba-hq-dataset
+mkdir -p datasets/temp
 
 unzip datasets/data256x256.zip -d datasets/celeba-hq-dataset/
 
 echo "Preparing Files..."
 # Reindex
+
 for i in `echo {00001..00030}`
 do
-    mv 'datasets/celeba-hq-dataset/data256x256/'$i'.jpg' 'datasets/celeba-hq-dataset/data256x256/'$[10#$i - 1]'.jpg'
+    mv 'datasets/celeba-hq-dataset/data256x256/'$i'.jpg' 'datasets/temp/'$i'.jpg'
 done
 
+for i in `echo {00001..00030}`
+do
+    mv 'datasets/temp/'$i'.jpg' 'datasets/celeba-hq-dataset/data256x256/'$[10#$i - 1]'.jpg'
+done
 
 echo "Preparing Splits..."
 # Split: split train -> train & val
